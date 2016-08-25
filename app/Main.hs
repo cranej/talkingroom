8f408ce69074b35c604e580c3a4156d4923cba5f
Crane Jin <crane@cranejin.com>
2016-08-25 22:57:17 +0800

    Render markdown in scotty.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
-import Web.Scotty
+import           Control.Applicative           ((<$>))
+import           Control.Monad.IO.Class        (liftIO)
+import qualified Data.ByteString               as B
+import           Data.ByteString.Char8         (pack, unpack)
+import           Data.Text.Lazy                (Text)
+import           Lib
+import           Text.Blaze.Html.Renderer.Text
+import           Web.Scotty
+
+renderMarkdownFile :: FilePath -> IO Text
+renderMarkdownFile path = (renderHtml . markdownToHtml .unpack) <$> B.readFile path
 
 main :: IO ()
 main = scotty 3000 $ do
     get "/" $ do
-        html "Hello World!"
+        c <- liftIO . renderMarkdownFile $ "readme.md"
+        html c
