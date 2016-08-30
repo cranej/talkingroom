6cfbdf7f36e2716df38ec6bdf9e7fad6c9ba017c
Crane Jin <crane@cranejin.com>
2016-08-30 21:48:00 +0800

    Add slugify

--- app/Lib.hs
+++ app/Lib.hs
@@ -1,11 +1,13 @@
 module Lib
     ( markdownToHtml
+    , slugify
     ) where
 
 import Text.Pandoc
 import Text.Pandoc.Error (handleError)
 import Text.Blaze.Html (Html)
 import Data.Set (Set)
+import Data.Char (isSpace, isLetter)
 import qualified Data.Set as Set
 
 markdownToHtml :: String -> Html
@@ -17,4 +19,12 @@ markdownToHtml =
 markdownExtensions :: Set Extension
 markdownExtensions =
   Set.union githubMarkdownExtensions $ Set.fromList [Ext_yaml_metadata_block, Ext_definition_lists]
+
+slugify :: String -> String
+slugify s = foldr f "" s
+  where
+    f c acc
+      | isSpace c = '-':acc
+      | isLetter c = c:acc
+      | otherwise = acc
 
