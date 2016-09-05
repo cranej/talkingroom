fc22a2b17b34b53dd87f0310d5d142bc27d7719c
Crane Jin <crane@cranejin.com>
2016-09-05 21:52:47 +0800

    Implement basic authentication for Scotty.
    Could not use HttpAuth middleware in 'wai-extra' as no easy way to only
    authenticate part of the sites.

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,17 @@ module Main where
 
 import           Backend
 import           Control.Applicative             ((<$>))
-import           Control.Monad                   (unless,when)
+import           Control.Monad                   (unless, when)
 import           Control.Monad.IO.Class          (liftIO)
+import           Crypto.Hash
 import qualified Data.ByteString                 as B
-import           Data.ByteString.Char8           (pack, unpack)
+import           Data.ByteString.Base64          (decodeLenient)
+import           Data.ByteString.Char8           (pack, unpack,split)
 import           Data.SecureMem
 import           Data.Text.Lazy                  (Text)
 import           Lib
 import           Network.HTTP.Types.Status       (unauthorized401)
-import           Network.Wai                     (Request)
-import           Network.Wai.Middleware.HttpAuth
+import           Network.Wai                     (Request, requestHeaders)
 import           Text.Blaze.Html.Renderer.Text
 import           Web.Scotty
 
@@ -23,21 +24,33 @@ getPostFilePath :: String -> FilePath
 getPostFilePath s = "testdata/" ++ s ++ ".md"
 
 password :: SecureMem
-password = secureMemFromByteString "password01!"
+password = secureMemFromByteString "54de7f606f2523cba8efac173fab42fb7f59d56ceff974c8fdb7342cf2cfe345"
+
+hashPassword :: B.ByteString -> B.ByteString
+hashPassword s = pack . show $ hs
+  where hs = (hash s) :: Digest SHA256
 
 auth :: (Request -> IO Bool) -> ActionM () -> ActionM ()
+
 auth doAuth action = do
     r <- request
     authenticated <- liftIO $ doAuth r
     unless authenticated $ do
         status unauthorized401
-        setHeader "WWW-Authenticate" "Basic realm=\"Incoming\""
+        setHeader "WWW-Authenticate" "Basic realm=\"Authentication Required\""
         html "<h1>Authentication required.</h1>"
     when authenticated action
 
 doAuth :: Request -> IO Bool
 doAuth r = do
-     return False
+    case [v | (k, v) <- requestHeaders r, k == "Authorization" ] of
+        [value] -> return $ check (decode value)
+        _       -> return False
+
+    where
+        decode = split ':' . decodeLenient . head . drop 1 . split ' '
+        check ["admin", pwd] = (secureMemFromByteString . hashPassword $ pwd) == password
+        check _ = False
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -45,8 +58,6 @@ main = scotty 3000 $ do
         fname <- getPostFilePath <$> param "post" `rescue` (\err -> return "index")
         c <- liftIO . renderMarkdownFile $ fname
         html c
-
-    -- middleware $ basicAuth (\u p -> return $ u == "user" && secureMemFromByteString p == password) "what is this for?"
 
     get "/admin/secret" $ auth doAuth $ do
         text "You must be authenticated!"
